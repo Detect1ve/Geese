a5cceead391f1b5d609c6339fd114b366513981e
Nikolai Grin <Nikolai_Grin@Mail.Ru>
2018-10-08 23:20:49 +0700

    Read values to convert degrees

--- lab1/task.rb
+++ lab1/task.rb
@@ -3,6 +3,18 @@
 def hello
         puts "Hello, World!"
 end
+	
+
+def degree_conversion
+	puts 'Input degrees'
+	degrees = gets.chomp
+
+	puts 'Intup name of the scale'
+	scale = gets.chomp
+
+	puts 'Input name of the conversion scale'
+	convertion_scale = gets.chomp
+end
 
 hello
-
+degree_conversion
