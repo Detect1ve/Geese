8657ea8cb636256618d47cf0c1cc10c8cc35ca7b
Nikolai Grin <Nikolai_Grin@Mail.Ru>
2018-10-23 17:51:13 +0700

    Creating a method for converting and returning correctly from a validation method

--- lab1/task.rb
+++ lab1/task.rb
@@ -1,27 +1,56 @@
 #!/usr/bin/env ruby
 
 def hello
-        puts "Hello, World!"
+	puts "Hello, World!"
 end
-	
+
+def conversion(from, where, how)
+	if (from == where)
+		return how
+	end
+	case (from + where)
+		when (-1)
+			if (from == -1)
+				return how + 273
+			else
+				return how - 273
+			end
+		when (1)
+			if (from == 0)
+				return 1.8 * (how - 273) + 32
+			else
+				return 1.8 * (how - 32) + 273
+			end
+		when (0)
+			if (from == -1)
+				return (how * 1.8) + 32
+			else
+				return (how - 32) * 1.8
+			end
+	end
+end
+
 def check(scale_or_not)
 	flag = 1
-        while flag == 1 do
-                case scale_or_not
-                        when ('C' or 'c')
-                                puts "Celsius scale"
-                                flag = 0
-                        when ('K' or 'k')
-                                puts "Kelvin scale"
-                                flag = 0
-                        when ('F' or 'f')
-                                puts "Fahrenheit scale"
-                                flag = 0
-                        else
-                                puts "You have entered incorrect data. Repeat input"
-                                scale_or_not = gets.chomp
-                end
-        end
+	while flag == 1 do
+		case scale_or_not
+			when ('C' or 'c')
+				puts "Celsius scale"
+				scale_or_not = -1
+				flag = 0
+			when ('K' or 'k')
+				puts "Kelvin scale"
+				scale_or_not = 0
+				flag = 0
+			when ('F' or 'f')
+				puts "Fahrenheit scale"
+				scale_or_not = 1
+				flag = 0
+			else
+				puts "You have entered incorrect data. Repeat input"
+				scale_or_not = gets.chomp
+			end
+	end
 	return scale_or_not
 end
 
@@ -36,6 +65,9 @@ def degree_conversion
 	puts 'Input name of the conversion scale'
 	convertion_scale = gets.chomp
 	convertion_scale = check(convertion_scale)
+
+	result = conversion(scale.to_i, convertion_scale.to_i, degrees.to_i)
+	puts result
 end
 
 hello
